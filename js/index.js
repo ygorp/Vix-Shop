6c12c66bd303c66791d7ecf2ae3edac181886841
ygorp <ygor-nogueira@hotmail.com>
2023-02-21 20:12:21 -0300

    criando site de vendas vixshop, foi feita a estrutura html até a seção services e o css responsivo

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.querySelector('#menu-btn').addEventListener('click', function(){
+
+    document.querySelector('#menu-site').classList.toggle("active");
+    document.querySelector('#menu-icon').classList.toggle("active");
+})
