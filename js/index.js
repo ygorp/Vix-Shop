f9f711fa68d111774af63d07d15723fc39a48467
ygorp <ygor-nogueira@hotmail.com>
2023-02-27 22:11:12 -0300

    implementando a página de contatos, feito toda a estilização e deixando responsivo

--- js/index.js
+++ js/index.js
@@ -3,3 +3,4 @@ document.querySelector('#menu-btn').addEventListener('click', function(){
     document.querySelector('#menu-site').classList.toggle("active");
     document.querySelector('#menu-icon').classList.toggle("active");
 })
+
